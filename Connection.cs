73941d3c8100789ffc74d9adcef1eb4e5133c2c0
Yana Herman <yanagerman2907@gmail.com>
2019-04-09 20:51:22 +0300

    Add CreateTable, DeleteTable

--- Connection.cs
+++ Connection.cs
@@ -19,14 +19,55 @@ namespace MyFile
             ConnectionString = connString;
         }
 
-        public TextReader GetDataReader(string TableName)
+        public TextDReader GetDataReader(string TableName)
         {
-            return new TextReader(this, TableName);
+            return new TextDReader(this, TableName);
         }
 
         public Table GetTable(string tableName)
         {
             return new Table(this, tableName);
+        }
+
+        
+        public void CreateTable(string tableName, string[] columns)
+        {
+            //need checking if table exists
+            using (StreamWriter streamWriter = new StreamWriter(ConnectionString, true))
+            {
+                streamWriter.WriteLine(string.Format("<{0}>", tableName));
+                streamWriter.WriteLine(string.Join(separator, columns));
+                streamWriter.WriteLine("<end>");
+            }
+        }
+
+        public void DeleteTable(string tableName)
+        {
+            List<string> lines = new List<string>();
+            using (StreamReader reader = new StreamReader(ConnectionString))
+            {
+                string line;
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+
+                    if (line != string.Format("<{0}>", tableName))
+                    {
+                        lines.Add(line);
+                    }
+                    else
+                    {
+                        while (reader.ReadLine() != "<end>") ;
+                    }
+                }
+            }
+            using (StreamWriter writer = new StreamWriter(ConnectionString))
+            {
+                foreach (string _line in lines)
+                {
+                    writer.WriteLine(_line);
+                }
+            }
         }
     }
 }
